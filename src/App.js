079d482b81de6c98639ae0d5514263667e84869b
Lane Z <lane@freshpaint.io>
2025-03-17 16:00:35 -0400

    small refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import useFreshpaintCDN from './useFreshpaintCDN';
+import useFreshpaint from './useFreshpaint';
 
 function App() {
-  const freshpaint = useFreshpaintCDN()
+  useFreshpaint()
   return (
 
     <div className="App">
